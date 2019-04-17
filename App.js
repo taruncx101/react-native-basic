20ff2c0c7b9189f38c356c6f51c98ffda9f42494
Tarun Gorai <amitabh@codelogicx.com>
2019-04-17 18:08:57 +0530

    touches and buttons basic 1

--- App.js
+++ App.js
@@ -6,74 +6,66 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Tarun Kumar Gorai,\n' +
-    ':)  :) :) :)',
-});
-
-type Props = {};
 
 
 export default class App extends Component {
   render() {
     return (
-        <PizzaTranslator/>
+        <ButtonBasics/>
     );
   }
 }
-class PizzaTranslator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: ''};
+class ButtonBasics extends Component {
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
     }
 
     render() {
         return (
-            <View style={{padding: 10}}>
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Type here to translate!"
-                    onChangeText={(text) => this.setState({text})}
-                />
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                        color="#841584"
+                    />
+                </View>
+                <View style={styles.alternativeLayoutButtonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="This looks great!"
+                    />
+                    <Button
+                        onPress={this._onPressButton}
+                        title="OK!"
+                        color="#841584"
+                    />
+                </View>
             </View>
         );
     }
 }
 
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-    bigBlue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
+    container: {
+        flex: 1,
+        justifyContent: 'center',
     },
-    red: {
-        color: 'red',
+    buttonContainer: {
+        margin: 20
     },
+    alternativeLayoutButtonContainer: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
 });
